484296b93384ce56333176fb413244010d817606
dyk000825 <kdy000825@naver.com>
2024-02-21 16:03:04 +0900

    commit message

---
+++ src/main/java/jpabook/jpashop/domain/item/Album.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@DiscriminatorValue("A")
+@Getter @Setter
+public class Album extends Item {
+    public String artist;
+    public String etc;
+}
---
+++ src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@DiscriminatorValue("B")
+@Getter@Setter
+public class Book extends Item {
+    public String author;
+    public String isbn;
+
+}
